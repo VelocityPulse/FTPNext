8c9ee72057880ebb2ce3136b3bd52642277398fe
Velocitypulse <velocitypulse@outlook.fr>
2019-01-18 15:27:50 +0100

    [MAIN] Add some test debug

--- app/src/main/java/com/example/ftpnext/MainActivity.java
+++ app/src/main/java/com/example/ftpnext/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ftpnext;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,27 +17,34 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static String TAG = "MAIN ACTIVITY";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        Toolbar lToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(lToolBar);
+
+        FloatingActionButton lFloatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        lFloatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "You have to be connected.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        DrawerLayout lDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle lToggle = new ActionBarDrawerToggle(
+                this, lDrawer, lToolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        lDrawer.addDrawerListener(lToggle);
+        lToggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -59,6 +67,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    // ID OF MENU BUTTONS
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -66,6 +75,7 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        Log.i(TAG, "id : " + id);
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -81,17 +91,18 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+            Log.i(TAG, "nav camera clicked");
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            Log.i(TAG, "nav gallery clicked");
         } else if (id == R.id.nav_slideshow) {
-
+            Log.i(TAG, "nav slideshow clicked");
         } else if (id == R.id.nav_manage) {
-
+            Log.i(TAG, "nav manage clicked");
         } else if (id == R.id.nav_share) {
-
+            Log.i(TAG, "nav share clicked");
         } else if (id == R.id.nav_send) {
-
+            Log.i(TAG, "nav send clicked");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
