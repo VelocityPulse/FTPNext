612ef7b7e5f275f45b8c02ebfcfc7a57d2e462f5
Velocitypulse <velocitypulse@outlook.fr>
2019-08-20 16:50:08 +0200

    [MAIN] Remove left main nav bar

--- app/src/main/java/com/example/ftpnext/MainActivity.java
+++ app/src/main/java/com/example/ftpnext/MainActivity.java
@@ -6,8 +6,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -45,8 +43,7 @@ Ideas :
     - Add a shortcut of a server on the desktop
  */
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "MAIN ACTIVITY";
 
@@ -124,31 +121,6 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            Log.i(TAG, "nav camera clicked");
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-            Log.i(TAG, "nav gallery clicked");
-        } else if (id == R.id.nav_slideshow) {
-            Log.i(TAG, "nav slideshow clicked");
-        } else if (id == R.id.nav_manage) {
-            Log.i(TAG, "nav manage clicked");
-        } else if (id == R.id.nav_share) {
-            Log.i(TAG, "nav share clicked");
-        } else if (id == R.id.nav_send) {
-            Log.i(TAG, "nav send clicked");
-        }
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
     private void initializeGUI() {
 
         Toolbar lToolBar = findViewById(R.id.toolbar);
@@ -165,14 +137,6 @@ public class MainActivity extends AppCompatActivity
                 startConfigureFTPServerActivity(ConfigureFTPServerActivity.NO_DATABASE_ID);
             }
         });
-
-        DrawerLayout lDrawer = findViewById(R.id.drawer_layout);
-
-        ActionBarDrawerToggle lToggle = new ActionBarDrawerToggle(
-                this, lDrawer, lToolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-
-        lDrawer.addDrawerListener(lToggle);
-        lToggle.syncState();
 
         RecyclerView lRecyclerView = findViewById(R.id.main_recycler_view);
         lRecyclerView.setLayoutManager(new LinearLayoutManager(this));
